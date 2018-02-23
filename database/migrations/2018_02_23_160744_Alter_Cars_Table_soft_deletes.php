b1ca037a8fb4d5d4789286983a533b816718e7ba
Haris Khan <haris.khan@gems.techverx.com>
2018-02-23 21:36:49 +0500

    Cars soft deletes

---
+++ database/migrations/2018_02_23_160744_Alter_Cars_Table_soft_deletes.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterCarsTableSoftDeletes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table("car_customer", function ($table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table("car_customer", function ($table) {
+            $table->dropSoftDeletes();
+        });
+    }
+}
