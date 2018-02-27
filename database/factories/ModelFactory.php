f44200f056a3b27ab4d7d3c6d2bcae57ca604d56
Shaban Hassan <shaban@gems.techverx.com>
2018-02-27 20:00:21 +0500

    Customer Cars API fixes after migrations changed

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -30,7 +30,7 @@ $factory->define(App\Customer::class, function (Faker\Generator $faker) {
         'name'          => $faker->name,
         'email'         => preg_replace('/@example\..*/', '@wmp.com', $faker->unique()->safeEmail),
         'password'      => $password ?: $password = bcrypt('secret'),
-        'con_number'    => '03'.$faker->randomElement(['00','01','02','03','04','05','06','07',10,11,12,13,14,15,16,20,21,22,23,24,25,26,31,32,33,34,35,41,42,43,44,45,46,47]).'-'.$faker->numberBetween(4,9).$faker->randomNumber(6, true),
+        'con_number'    => '03'.$faker->randomElement(['00','01','02','03','04','05','06','07',10,11,12,13,14,15,16,20,21,22,23,24,25,26,31,32,33,34,35,41,42,43,44,45,46,47]).$faker->numberBetween(4,9).$faker->randomNumber(6, true),
         'remember_token'=> str_random(10),
         'created_at'    => $faker->dateTimeBetween('-5 days', 'now', 'Asia/Karachi'),
         'updated_at'    => $faker->dateTimeBetween('-3 days', 'now', 'Asia/Karachi'),
