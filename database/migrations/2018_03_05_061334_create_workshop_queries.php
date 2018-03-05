1fd549b77a9ee3be0b0db5b3dc4d4ec33697693d
Haris Khan <haris.khan@gems.techverx.com>
2018-03-05 11:37:43 +0500

    workshop queries migration

---
+++ database/migrations/2018_03_05_061334_create_workshop_queries.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorkshopQueries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workshop_queries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('workshop_id')->unsigned();
+            $table->foreign('workshop_id')->references('id')->on('workshops')->onDelete('cascade');
+            $table->string('subject');
+            $table->text('message');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('workshop_queries', function (Blueprint $table) {
+            $table->dropForeign('workshop_queries_customer_id_foreign');
+        });
+        Schema::dropIfExists('workshop_queries');
+    }
+}
