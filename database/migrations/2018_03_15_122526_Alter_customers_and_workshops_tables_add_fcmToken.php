acfc69077d246efa673794748c5cc6e413e60fed
Haris Khan <haris.khan@gems.techverx.com>
2018-03-15 18:38:23 +0500

    Alter table for FCM token

---
+++ database/migrations/2018_03_15_122526_Alter_customers_and_workshops_tables_add_fcmToken.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterCustomersAndWorkshopsTablesAddFcmToken extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->string('fcm_token');
+        });
+        Schema::table('workshops', function (Blueprint $table) {
+            $table->string('fcm_token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('workshops', function (Blueprint $table) {
+            $table->dropColumn('fcm_token');
+        });
+        Schema::table('customers', function (Blueprint $table) {
+            $table->dropColumn('fcm_token');
+        });
+    }
+}
