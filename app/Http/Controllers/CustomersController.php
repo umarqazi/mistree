299b93cf98bcca920f68c5ebf18b9ff1347c7a4c
Haris Khan <haris.khan@gems.techverx.com>
2018-03-24 21:08:28 +0500

    Field name change

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -1227,7 +1227,8 @@ class CustomersController extends Controller
     * @param int $id 
     * @return \Illuminate\Http\Response 
     */ 
-    public function updateProfileImage(Request $request) { 
+    public function updateProfileImage(Request $request)
+    {
         $customer       = JWTAuth::authenticate(); 
         $file_data      = $request->profile_pic;
 
@@ -1240,7 +1241,7 @@ class CustomersController extends Controller
         $full_path = $customers_path.$customer->id.'/logo/'.md5(microtime()).".jpg";
         $url = $this->upload_image($file_data,$customer->id,$full_path);
         $url = url('/').'/'.$specified_customer_path.'/logo/'.basename($url);
-        $customer->profile_pic = $url;
+        $customer->profile_pic_url = $url;
         $customer->save();
 
         return response()->json([
