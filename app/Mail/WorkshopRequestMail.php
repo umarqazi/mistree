deda43c768e56bfcccd0d2a992ad6d4d0120e81a
Haris Khan <haris.khan@gems.techverx.com>
2018-04-02 17:14:12 +0500

    New mailable

---
+++ app/Mail/WorkshopRequestMail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class WorkshopRequestMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $dataMail;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($dataMail)
+    {
+        $this->dataMail = $dataMail;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $dataMail = $this->dataMail;
+        $workshop = $dataMail['workshop'];
+        return $this->view($dataMail['view'])
+            ->from($workshop->email, $workshop->name)
+            ->subject($dataMail['subject'])
+            ->with([
+                'workshop' => $workshop,
+                'subject' => $dataMail['subject'],
+                'msg' => $dataMail['msg']
+            ]);
+    }
+}
