9a23ce9c74b0c2beb6ab0bf037bf7a6ef516db79
Haris Khan <haris.khan@gems.techverx.com>
2018-04-06 17:57:55 +0500

    Post url to get url.

--- routes/web.php
+++ routes/web.php
@@ -144,8 +144,8 @@ Route::group(['prefix' => 'admin', 'middleware' => 'workshop.guest'], function (
         Route::get('/top-up', 'WorkshopsController@topup');
         Route::post('/update-balance', 'WorkshopsController@topupBalance');
 
-        Route::get('/booking', 'BookingsController@bookingListings');
-        Route::post('/booking/', 'BookingsController@bookingListings');
+//        Route::get('/booking/', 'BookingsController@bookingListings');
+        Route::get('/booking/{type}', 'BookingsController@bookingListings');
         
         
         Route::get('/authorized-workshops', 'WorkshopsController@authorized');
